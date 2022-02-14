43645222079e1a952e2d95dd126946723438ea14
Shah Jahidul Islam Riad <shahriad555@gmail.com>
2022-02-14 21:50:57 +0600

    normal commit

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -44,7 +44,7 @@ const PlaceDetails = ({ place }) => {
                         </Typography>
                     </Box>
                     <Box display="flex" justifyContent="space-between">
-                        <Typography variant="subtitle1">Ranking</Typography>
+                        <Typography variant="subtitle1">Ranking.</Typography>
                         <Typography gutterBottom variant="subtitle1">
                             {place.ranking}
                         </Typography>
